0a591bbe5902664f515faaaea7304c65aed474e4
Ben Meijering <bitwise.ben@gmail.com>
2016-03-31 09:10:05 +0200

    Added game of snake

--- x/environment.py
+++ x/environment.py
@@ -5,23 +5,23 @@ from __future__ import division
 import numpy as np
 
 
-class Enviroment(object):
-    """Base Enviroment class
+class Environment(object):
+    """Base Environment class
 
     Attributes
     ----------
     observe : callable
-        Get observation of the Enviroment's state
+        Get observation of the Environment's state
     update : callable
-        Update Enviroment's state from a given action
+        Update Environment's state from a given action
     reset : callable
-        Reset Enviroment to an initial state
+        Reset Environment to an initial state
     reward : callable
         Reward in the current state, returns (new_observation, reward, is_over)
     is_over : bool
-        If the Enviroment is in a terminal state
+        If the Environment is in a terminal state
     state : list
-        Inner representation of the Enviroment state
+        Inner representation of the Environment state
 
     """
     def __init__(self):
@@ -33,22 +33,26 @@ class Enviroment(object):
     def update(self, action):
         raise NotImplementedError
 
-    def reset(self, action):
+    def reset(self):
         raise NotImplementedError
 
-    def reward(self, action):
+    def reward(self):
         raise NotImplementedError
 
     @property
-    def is_over(self, action):
+    def is_over(self):
         raise NotImplementedError
 
     @property
     def state(self):
         raise NotImplementedError
 
+    @property
+    def description(self):
+        raise NotImplementedError
 
-class Catcher(Enviroment):
+
+class Catcher(Environment):
     """Catcher
 
     Agent is a 3 pixel `basket` in the bottom of a square grid of size
@@ -154,3 +158,99 @@ class Catcher(Enviroment):
     @property
     def description(self):
         return "Catch game with grid size {}".format(self.grid_size)
+
+
+class Snake(Environment):
+
+    _apa = [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)]  # All possible actions
+    _grow = 0  # Snake does not yet need to grow any segments
+
+    def __init__(self, grid_size, snake_length=3):
+        self.grid_size = grid_size
+        self.snake_length = snake_length
+
+        xs = range(1, grid_size - 1)  # X and y coordinates of empty cells
+        self._inner_cells = [(x, y) for x in xs for y in xs]
+
+        self.reset()
+
+    def _rand_empty_cell(self):
+        empties = set(self._inner_cells)
+        empties.difference_update(set(self.snake))
+        empties = list(empties)
+        np.random.shuffle(empties)
+        return empties[0]
+
+    def observe(self):
+        frame = np.zeros((self.grid_size, ) * 2)
+        frame[[0, -1]] = 1.
+        frame[:, [0, -1]] = 1.
+        for segment in self.snake:
+            frame[segment[1], segment[0]] = 1.
+        frame[self.fruit[1], self.fruit[0]] = .5
+        return frame
+
+    def update(self, action):
+        if self.is_over:
+            raise RuntimeWarning, 'Game over'
+        if action >= len(self._apa):
+            raise ValueError, 'Action not available'
+
+        action = self._apa[action]
+        if not sum(action):
+            action = self.actions[0]  # Repeat previous action
+        self.actions.insert(0, action)
+        self.actions.pop()
+
+        if self._grow > 0:
+            self.snake.append(self.snake[-1])
+            self.actions.append((0, 0))
+            self._grow -= 1
+
+        for ix, act in enumerate(self.actions):
+            x, y = self.snake[ix]
+            delta_x, delta_y = act
+            self.snake[ix] = x + delta_x, y + delta_y
+
+        self._reward = 0
+
+        # Snake either ate itself, or hit into wall
+        if len(self.snake) > len(set(self.snake)):
+            self._reward = -1
+        elif not set(self.snake).issubset(set(self._inner_cells)):
+            self._reward = -1
+
+        if self.fruit in self.snake:
+            self._grow += 1
+            self._reward = len(self.snake) - self.snake_length + 1
+            self.fruit = self._rand_empty_cell()
+
+        return self.state, self.reward(), self.is_over
+
+    def reset(self):
+        center = self.grid_size // 2
+        self.snake = [(x, center) for x in range(center, center + self.snake_length)]
+        self.actions = [(-1, 0)] * self.snake_length
+        self.fruit = self._rand_empty_cell()
+
+    def reward(self):
+        if not hasattr(self, '_reward'):
+            self._reward = 0
+
+        return self._reward
+
+    @property
+    def is_over(self):
+        if self.reward() < 0:
+            return True
+        return False
+
+    @property
+    def state(self):
+        return self.observe()
+
+    @property
+    def description(self):
+        return "Snake game with grid size {}".format(self.grid_size)
+
+
